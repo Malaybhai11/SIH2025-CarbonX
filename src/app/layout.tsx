2a9a1a8a31ab1235b0ec72458c77dc82b2745f51
Malaybhai11 <malay.raval11@gmail.com>
2025-09-21 09:27:14 +0530

    addind lotties and redesigning landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
-import { ClerkProvider, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
@@ -32,28 +31,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
         >
-          {/* Navbar */}
-          <header className="flex justify-between items-center p-4 h-16 bg-black text-white border-b border-gray-800">
-            <h1 className="text-xl font-bold text-teal-400">CarbonX</h1>
-            <div className="flex gap-4">
-              <SignedOut>
-                <Link href="/sign-in">
-                  <button className="bg-green-600 hover:bg-green-700 text-white rounded-md px-4 py-2 text-sm sm:text-base">
-                    SignIn
-                  </button>
-                </Link>
-                <Link href="/sign-up">
-                  <button className="bg-green-600 hover:bg-green-700 text-white rounded-md px-4 py-2 text-sm sm:text-base">
-                    Sign Up
-                  </button>
-                </Link>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </div>
-          </header>
-
           {/* Page Content */}
           {children}
         </body>
